f1d0fa5dba8f0d3546c436def75493421a28ce7a
Jugal Manjeshwar <jugalm9@gmail.com>
2013-09-09 11:12:59 -0400

    Redesign Intial

--- ce-mob/js/main.js
+++ ce-mob/js/main.js
@@ -1,3 +1,16 @@
 window.featuredEventSlider = Swipe(document.getElementById('featuredEventSlider'),
-	{auto:2000}
-);
+	{
+		auto:2000,
+		continuous: true,
+	    callback: function(pos) {
+
+	      var i = bullets.length;
+	      while (i--) {
+	        bullets[i].className = ' ';
+	      }
+	      bullets[pos].className = 'on';
+
+	    }
+	});
+
+var bullets = document.getElementById('position').getElementsByTagName('li');
